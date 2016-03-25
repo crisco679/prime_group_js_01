ccad914baca24029911cd915cb7d11fbeb6131a8
christuttle <crunch.tuttle@gmail.com>
2016-03-25 17:12:04 -0500

    change array to object

--- group_01.js
+++ group_01.js
@@ -1,63 +1,79 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+function Person(name,empNum,salary,rating){
+  this.name=name;
+  this.empNum=empNum;
+  this.salary=salary;
+  this.rating=rating
+}
+
+var atticus = new Person('Atticus','2405','47000',3)
+var jem = new Person('Jem','62347','63500',4)
+var boo = new Person('Boo', '11435','54000',3)
+var scout = new Person('Scout','6243','74750',5)
+
+// var atticus = ["Atticus", "2405", "47000", 3];
+// var jem = ["Jem", "62347", "63500", 4];
+// var boo = ["Boo", "11435", "54000", 3];
+// var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
 
 function calcSti(employee) {
-  var result = [];
-  for(var i = 0; i < employee.length;i++){
+  var result = {};
+
+
     // Setting employee name
 
-    result[0] = employee[i][0];
+    result.name = employee.name;
 
      // Checking employee rating
 
-    switch (employee[i][3]) {
+    switch (employee.rating) {
       case 3:
-        result[1]=.04;
+        result.percSti=.04;
         break;
       case 4:
-        result[1]=.06;
+        result.percSti=.06;
         break;
       case 5:
-        result[1]= .1;
+        result.percSti= .1;
         break;
       default:
-        result[1]= 0;
+        result.percSti= 0;
       }
 
       // Additional bonus % based on length at company
-      if (employee[i][1].length <= 4) {
-        result[1] += .05;
+      if (employee.empNum.length <= 4) {
+        result.percSti += .05;
       }
 
       // Remove 1% if income is over $65,000
-      if (employee[i][2] > 65000) {
-        result[1] -= .01;
+      if (employee.salary > 65000) {
+        result.percSti -= .01;
       }
 
       // If bonus is over 13% lower set it to 13%
-      if (result[1] > .13) {
-        result[1] = .13;
+      if (result.percSti > .13) {
+        result.percSti = .13;
       }
 
       // Annual Income + STI
-      var sti = parseInt(employee[i][2])*result[1];  //calculates STI
-      result[2]= sti + parseInt(employee[i][2]); // adds income and STI
+      var sti = parseInt(employee.salary)*result.percSti;  //calculates STI
+      result.annComp= sti + parseInt(employee.salary); // adds income and STI
       //console.log("Income + STI = " + result[2]);
 
       //Total bonus
       // Math.round()
-      result[3] = Math.round(employee[i][2] * result[1]);
+      result.bonus = Math.round(employee.salary * result.percSti);
 
 
       // Convert decimal to percentage string with %
-      result[1] = result[1] * 100 + "%";
+      result.percSti = result.percSti * 100 + "%";
 
       // Final result
       console.log(result);
   }
+
+for(var it=0;it<employees.length;it++){
+calcSti(employees[it]);
+
 }
-calcSti(employees);
